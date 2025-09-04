18f4ae7a292057ce94121256416c7cb69ba96d0c
ArnoldRamodike <riccoarnold23@gmail.com>
2025-09-04 19:10:28 +0200

    Update healt endpoint

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -1,7 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/sikozonpc/goserve/internal/store"
+)
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("ok"))
+
+	app.store.Posts.Create(r.Context(), &store.Post{})
 }
